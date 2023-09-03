db1c5c30275112d3a245daa33992a537955b2ce9
hugo ludolff <hugoludovisck@gmail.com>
2023-09-03 17:41:01 -0300

    got it working

---
+++ main.py
@@ -0,0 +1,63 @@
+from menu import menu, menu_numero
+import math
+
+def checker(number):
+    if number <= 3:
+        return True
+    if number % 2 == 0 or number % 3 == 0:
+        return False
+    i = 5
+    while i * i <= number:
+        if number % i == 0 or number % (i + 2) == 0:
+            return False
+        i+=6
+    return True
+
+def find_divisors(number):
+    divisors = []
+    for divisor in range(1, number + 1):
+        if number % divisor == 0:
+            divisors.append(divisor)
+    return divisors
+
+
+def printer(number):
+    if checker(number) == True:
+        print(f'{number} is a prime number')
+    else:
+        divisors = find_divisors(number)
+        print(f"{number} is not a prime number. It is divisible by {', ' .join(map(str, divisors))}.")
+
+
+def function(number):
+    while True:
+        number = menu_numero()
+        if number < 0:
+            print('Negative numbers wont work')
+        elif number == 0:
+            break
+        elif number == 1:
+            print('One')
+        else: 
+            printer(number)
+    
+
+
+def main():
+    number = 1
+    while True:
+        option = menu()
+        if option == '1':
+            function(number)
+        elif option == '2':
+            print('Quiting...')
+            break
+        else: 
+            print('Invalid Input!')
+
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ menu.py
@@ -0,0 +1,18 @@
+def menu():
+    menu = """
+===========MENU==========
+[1] - Start
+[2] - Quit
+=========================
+--> """
+    return input(menu)
+
+
+def menu_numero():
+    menu_numero = """
+=======NUMBER=========
+Enter a positive number 
+or use 0(zero) to return to main menu
+======================
+-> """
+    return int(input(menu_numero))
